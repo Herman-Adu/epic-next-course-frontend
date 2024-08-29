73baf4729ec7985d65377e2c31877bddb8f12463
Herman Adu <herman.adu@hotmail.com>
2024-08-29 15:42:57 +0100

    Implement OpenAI and langchain to generate YouTube transcript summary and pull it into the application

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest } from "next/server";
 
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
 import { fetchTranscript } from "@/lib/youtube-transcript";
 import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 import { getAuthToken } from "@/data/services/get-token";
@@ -17,7 +21,50 @@ function transformData(data: any[]) {
   };
 }
 
-// if we make aa call to /api/summarise - it's going to hit this endpoint
+const TEMPLATE = `
+INSTRUCTIONS: 
+  For the this {text} complete the following steps.
+  Generate the title based on the content provided
+  Summarize the following content and include 5 key topics, writing in first person using normal tone of voice.
+  
+  Write a youtube video description
+    - Include heading and sections.  
+    - Incorporate keywords and key takeaways
+
+  Generate bulleted list of key points and benefits
+
+  Return possible and best recommended key words
+`;
+
+async function generateSummary(content: string, template: string) {
+  const prompt = PromptTemplate.fromTemplate(template);
+
+  const model = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: process.env.OPENAI_MODEL ?? "gpt-4-turbo-preview",
+    temperature: process.env.OPENAI_TEMPERATURE
+      ? parseFloat(process.env.OPENAI_TEMPERATURE)
+      : 0.7,
+    maxTokens: process.env.OPENAI_MAX_TOKENS
+      ? parseInt(process.env.OPENAI_MAX_TOKENS)
+      : 4000,
+  });
+
+  const outputParser = new StringOutputParser();
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  try {
+    const summary = await chain.invoke({ text: content });
+    return summary;
+  } catch (error) {
+    if (error instanceof Error)
+      return new Response(JSON.stringify({ error: error.message }));
+    return new Response(
+      JSON.stringify({ error: "Failed to generate summary." })
+    );
+  }
+}
+
 export async function POST(req: NextRequest) {
   console.log("FROM OUR ROUTE HANDLER:", req.body);
 
@@ -40,26 +87,26 @@ export async function POST(req: NextRequest) {
     );
 
   const body = await req.json();
-  const videoId = body.videoId;
+  const { videoId } = body;
 
+  // get transcript from youtube
   let transcript: Awaited<ReturnType<typeof fetchTranscript>>;
 
   try {
     transcript = await fetchTranscript(videoId);
 
     const transformedData = transformData(transcript);
-    console.log("Transcript: ", transformedData);
+    console.log("Transcript:", transformedData.text);
 
-    return new Response(
-      JSON.stringify({ data: "return from our handler", error: null }),
-      {
-        status: 200,
-      }
-    );
+    let summary: Awaited<ReturnType<typeof generateSummary>>;
+
+    summary = await generateSummary(transformedData.text, TEMPLATE);
+    console.log("Summary:", summary);
+    return new Response(JSON.stringify({ data: summary, error: null }));
   } catch (error) {
     console.error("Error processing request:", error);
     if (error instanceof Error)
-      return new Response(JSON.stringify({ error: error.message }));
-    return new Response(JSON.stringify({ error: "Error getting transcript." }));
+      return new Response(JSON.stringify({ error: error }));
+    return new Response(JSON.stringify({ error: "Unknown error" }));
   }
 }
